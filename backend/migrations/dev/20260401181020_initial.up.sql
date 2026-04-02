7308fa5dd27a343a97b2d5865c8c20dfcfb09f5b
Megidy <byrka.sergiy@gmail.com>
2026-04-02 23:52:41 +0300

    feat: implement initial backend structure with user authentication and inventory management
    - Added configuration management for environment variables.
    - Implemented HTTP router with health check and Swagger documentation.
    - Created authentication controller with login and user retrieval endpoints.
    - Developed inventory controller for paginated inventory retrieval.
    - Introduced JWT middleware for authentication and pagination middleware.
    - Defined DTOs for HTTP requests and responses.
    - Established entity models for users, resources, inventories, and delivery requests.
    - Created PostgreSQL repositories for user and inventory data access.
    - Implemented use cases for authentication and inventory management.
    - Added database migration scripts for initial schema setup.
    - Included Makefile for easy development commands.

---
+++ backend/migrations/dev/20260401181020_initial.up.sql
@@ -0,0 +1,108 @@
+CREATE TYPE request_priority AS ENUM ('normal', 'elevated', 'critical', 'urgent');
+CREATE TYPE allocation_status AS ENUM('planned', 'approved', 'in_transit', 'delivered', 'cancelled');
+CREATE TYPE request_status AS ENUM ('pending', 'allocated', 'in_transit', 'delivered', 'cancelled');
+CREATE TYPE user_role AS ENUM ('worker', 'admin');
+CREATE TYPE customer_type AS ENUM ('shop', 'mall');
+
+CREATE TABLE warehouses (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE customers (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type customer_type NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role user_role NOT NULL,
+    warehouse_id BIGINT REFERENCES warehouses(id) ON DELETE SET NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE resources (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(100),
+    unit_measure VARCHAR(50),
+    logo_uri TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE inventories (
+    id BIGSERIAL PRIMARY KEY,
+    warehouse_id BIGINT NOT NULL REFERENCES warehouses(id) ON DELETE CASCADE,
+    resource_id BIGINT NOT NULL REFERENCES resources(id) ON DELETE CASCADE,
+    quantity DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    UNIQUE (warehouse_id, resource_id)
+);
+
+CREATE TABLE delivery_requests (
+    id BIGSERIAL PRIMARY KEY,
+    destination_id BIGINT NOT NULL REFERENCES customers(id) ON DELETE CASCADE,
+    resource_id BIGINT NOT NULL REFERENCES resources(id) ON DELETE CASCADE,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    quantity DOUBLE PRECISION NOT NULL,
+    priority request_priority NOT NULL,
+    status request_status NOT NULL,
+    estimated_arrival_at TIMESTAMPTZ
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE allocations (
+    id BIGSERIAL PRIMARY KEY,
+    request_id BIGINT NOT NULL REFERENCES delivery_requests(id) ON DELETE CASCADE,
+    source_warehouse_id BIGINT NOT NULL REFERENCES warehouses(id) ON DELETE CASCADE,
+    quantity DOUBLE PRECISION NOT NULL,
+    allocation_status allocation_status NOT NULL, 
+    dispatched_at TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE OR REPLACE FUNCTION update_timestamp_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = CURRENT_TIMESTAMP;
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER set_timestamp_warehouses
+BEFORE UPDATE ON warehouses FOR EACH ROW EXECUTE FUNCTION update_timestamp_column();
+
+CREATE TRIGGER set_timestamp_customers
+BEFORE UPDATE ON customers FOR EACH ROW EXECUTE FUNCTION update_timestamp_column();
+
+CREATE TRIGGER set_timestamp_resources
+BEFORE UPDATE ON resources FOR EACH ROW EXECUTE FUNCTION update_timestamp_column();
+
+CREATE TRIGGER set_timestamp_inventories
+BEFORE UPDATE ON inventories FOR EACH ROW EXECUTE FUNCTION update_timestamp_column();
+
+CREATE TRIGGER set_timestamp_delivery_requests
+BEFORE UPDATE ON delivery_requests FOR EACH ROW EXECUTE FUNCTION update_timestamp_column();
+
+CREATE TRIGGER set_timestamp_allocations
+BEFORE UPDATE ON allocations FOR EACH ROW EXECUTE FUNCTION update_timestamp_column();
+
+CREATE TRIGGER set_timestamp_users
+BEFORE UPDATE ON users FOR EACH ROW EXECUTE FUNCTION update_timestamp_column();
